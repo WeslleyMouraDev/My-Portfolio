b5e7d6e626e64b2948e5c157809366c0c90ccf52
Weslley Moura <schno@WeslleyMoura.localdomain>
2023-06-08 17:08:42 -0300

    refactor: Refatorando todo o portifolio para Next.js + Tailwind + Typescript. Também cria a barra de navegação

---
+++ components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client" // this is a client component
+import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll/modules"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+import Image from 'next/image'
+import navLogo from '../public/navLogo.png'
+
+interface NavItem {
+    label: string
+    page: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: "Inicio",
+        page: "inicio"
+    },
+    {
+        label: "Sobre",
+        page: "sobre"
+    },
+    {
+        label: "Projetos",
+        page: "projetos"
+    },
+]
+
+const Navbar = () => {
+    const { systemTheme, theme, setTheme } = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
+    const [navbar, setNavbar] = useState(false)
+
+
+
+    return (
+    // <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50">
+        <div className="justify-between md:items-center md:flex">
+            <div>
+                <div>
+                    <Image src={navLogo} alt="/" width={100} />
+                </div>
+            </div>
+            <div>
+                {NAV_ITEMS.map((item, idx) => {
+                    return <a key={idx}>{item.label}</a>
+                })}
+            </div>
+        </div>
+    </header>
+    )
+}
+
+export default Navbar
