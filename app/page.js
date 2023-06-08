a47003cb50dfc132d880ce917a7a22db96b8bb05
Weslley Moura <schno@WeslleyMoura.localdomain>
2023-06-08 13:35:49 -0300

    feat: Adiciona nav e personaliza um poucoo cabeçalho da página

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
-import Image from 'next/image'
+import { BsFillMoonStarsFill } from 'react-icons/bs';
 
 export default function Home() {
   return (
 
-    <main>
-
+    <main className='bg-white px-10'>
+      <section className='min-h-screen'>
+        <nav className='py-10 mb-12 flex justify-between'>
+        <h1 className='text-xl font-burtons'>Hello</h1>
+        <ul className='flex items-center'>
+          <li>
+            <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+          </li>
+          <li><a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Currículo</a></li>
+        </ul> 
+        </nav>
+          
+      </section>
     </main>
 
   )
