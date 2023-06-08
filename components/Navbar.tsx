ee2ad9df14fb272cdf04a23e199afbff8ec4f2b4
Weslley Moura <schno@WeslleyMoura.localdomain>
2023-06-08 19:20:49 -0300

    feat: Finaliza a barra de navegação e torna responsivo para mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
 
   return (
     <header
-      className={`w-full mx-auto px-4 sm:px-5 fixed top-0 z-50 shadow bg-white ${
+      className={`w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white ${
         currentTheme === "dark"
           ? "dark:bg-stone-900 dark:border-b dark:border-stone-600"
           : ""
@@ -44,37 +44,62 @@ const Navbar = () => {
     >
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div>
+          <div className="flex items-center justify-between py-3">
             <div className="md:py-5 md:block">
               <Image src={navLogo} alt="/" width={80} />
             </div>
-            <div>
-              <button>
-                {navbar ? <IoMdClose /> : <IoMdMenu />}
+            <div className="md:hidden">
+              <button onClick={() => setNavbar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
               </button>
             </div>
           </div>
         </div>
-        <div className="md:flex md:items-center md:space-x-6">
-          {NAV_ITEMS.map((item, idx) => {
-            return <a key={idx}>{item.label}</a>;
-          })}
 
-          {currentTheme === "dark" ? (
-            <button
-              onClick={() => setTheme("light")}
-              className="bg-slate-100 p-2 rounded-xl md:items-center"
-            >
-              <BsSun size={25} color="black"/>
-            </button>
-          ) : (
-            <button
-              onClick={() => setTheme("dark")}
-              className="bg-slate-100 p-2 rounded-xl md:items-center"
-            >
-              <BsMoonStarsFill />
-            </button>
-          )}
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    to={item.page}
+                    className={
+                      `block lg:inline-block ${currentTheme === "dark" ? "text-white" : "text-neutral-900"} hover:text-neutral-500`
+                    }
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                )
+              })}
+
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl md:items-center"
+                >
+                  <BsSun size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl md:items-center"
+                >
+                  <BsMoonStarsFill />
+                </button>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </header>
