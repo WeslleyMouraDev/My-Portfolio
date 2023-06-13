b1787116156b9ccd48207888fa3019dd1e7a1c58
WeslleyMouraDev <weslleymouradev@gmail.com>
2023-06-13 02:58:51 -0300

    fix: corrige link do curriculo e use-client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,35 @@
-"use client"
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import { ThemeProvider } from 'next-themes'
+'use client';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { ThemeProvider } from 'next-themes';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Weslley Moura - Fullstack Developer',
   description: 'Fullstack Developer',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider defaultTheme="light" enableSystem={true} attribute='class'>
+        <ThemeProvider
+          defaultTheme="light"
+          enableSystem={true}
+          attribute="class"
+        >
           <Navbar />
           {children}
           <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
